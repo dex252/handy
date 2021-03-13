5bc5717c0e066c7a1f67f341ca7dda2a6425f914
Slava <dex252rus@gmail.com>
2021-03-13 17:06:49 +1000

    Init

---
+++ HandyHost/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace HandyHost.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private ILogger<TestController> Logger { get; }
+
+        public TestController(ILogger<TestController> logger)
+        {
+            Logger = logger;
+        }
+
+        [HttpGet("hello/{name}")]
+        public IActionResult Method(string name)
+        {
+            return Ok($"Hello, {name}!");
+        }
+    }
+}
