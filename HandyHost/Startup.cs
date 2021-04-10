4527d0811fd6a827b9fc4ac24f66e250721ba15a
Slava <dex252rus@gmail.com>
2021-04-10 12:27:58 +1000

    SmallFix

--- HandyHost/Startup.cs
+++ HandyHost/Startup.cs
@@ -22,24 +22,18 @@ namespace HandyHost
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-	    System.Console.WriteLine("START APP");
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-	    System.Console.WriteLine("DEVELOP");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 	    
-          
-
             app.UseRouting();
 
             app.UseAuthorization();
